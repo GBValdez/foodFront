bfcddb9c185ba0661ef3b5210a40e3c728b357e1
GBValdez <gabrielbenjaminvaldezdeleon@gmail.com>
2024-05-19 22:50:51 -0500

    Puse el host del back

--- src/app/shared/constants/urls.ts
+++ src/app/shared/constants/urls.ts
@@ -1,6 +1,8 @@
 import { environment } from 'src/environments/environment';
 
-const BASE_URL = environment.production ? '' : 'http://localhost:5223';
+const BASE_URL = environment.production
+  ? 'https://backfood-ukjv.onrender.com'
+  : 'https://backfood-ukjv.onrender.com';
 
 export const FOODS_URL = BASE_URL + '/api/foods';
 export const FOODS_TAGS_URL = FOODS_URL + '/tags';
